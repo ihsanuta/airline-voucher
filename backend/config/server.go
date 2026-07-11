6599b5cad18307568d2956e0c9936a23b999a6bb
ihsan.maulana1 <ihsan.maulana@teknologi.belajar.id>
2026-07-11 13:03:33 +0700

    chore: allow cors

--- backend/config/server.go
+++ backend/config/server.go
@@ -1,12 +1,20 @@
 package config
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func InitEcho() *echo.Echo {
 	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:5173"},
+		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+	}))
 
 	// Middleware
 	e.Use(middleware.RequestLogger())
